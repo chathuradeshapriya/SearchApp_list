3adc6aa45b6f236426da5da873c488c785396e53
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-08-30 09:50:53 +0530

    Connect with REST Api

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,39 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:searchapp_list/search.dart';
+import 'package:searchapp_list/usermodel.dart';
+
+import 'api_services.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  FetchUserList _userList = FetchUserList();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-
       child: Scaffold(
-        body: Container(
-          child: ListView.builder(
-              itemBuilder: (context, index){
-                return Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Container(height: 60, width: 60,
+        appBar: AppBar(
+          title: Text('UserList'),
 
-                            decoration: BoxDecoration(
-                              color: Colors.purpleAccent,
-                              borderRadius: BorderRadius.circular(10.0)
-                            ),)
-                      ],
-                    ),
-                  ),
-                );
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: FutureBuilder<List<Userlist>>(
+              future: _userList.getuserList(),
+              builder: (context, snapshot) {
+                var data = snapshot.data;
+                return ListView.builder(
+                    itemCount: data?.length,
+                    itemBuilder: (context, index) {
+                      if (!snapshot.hasData) {
+                        return Container(
+
+                            child: Center(child: CircularProgressIndicator()));
+                      }
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.deepPurpleAccent,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '${data?[index].id}',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 20),
+                                Column(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${data?[index].name}',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        '${data?[index].email}',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ])
+                              ],
+                            ),
+                            // trailing: Text('More Info'),
+                          ),
+                        ),
+                      );
+                    });
               }),
         ),
-
-      )
+      ),
     );
   }
 }
